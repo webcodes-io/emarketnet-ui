078cd9f740987a7f700c6d929a2b86d1898327c7
Vasiliy Malakhov <mvamgc@gmail.com>
2018-10-15 20:26:36 -0400

    #4 - create product - cucumber configuration, jasmine e2e is removed

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -1,28 +1,39 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-const { SpecReporter } = require('jasmine-spec-reporter');
-
 exports.config = {
   allScriptsTimeout: 11000,
   specs: [
-    './src/**/*.e2e-spec.ts'
+    './features/*.feature'
   ],
   capabilities: {
     'browserName': 'chrome'
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
-  framework: 'jasmine',
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000,
-    print: function() {}
+  framework: 'custom',
+  frameworkPath: require.resolve('protractor-cucumber-framework'),
+  cucumberOpts: {
+    // require step definition files before executing features
+    require: ['./e2e/steps/**/*.ts'],
+    // <string[]> (expression) only execute the features or scenarios with tags matching the expression
+    tags: [],
+    // <boolean> fail if there are any undefined or pending steps
+    strict: true,
+    // <string[]> (type[:path]) specify the output format, optionally supply PATH to redirect formatter output (repeatable)
+    format: [
+      // 'pretty',
+      // 'pretty:reports/summary.txt',
+      'json:reports/summary.json'
+    ],
+    // <boolean> invoke formatters without executing steps
+    dryRun: false,
+    // <string[]> ("extension:module") require files with the given EXTENSION after requiring MODULE (repeatable)
+    compiler: []
   },
   onPrepare() {
     require('ts-node').register({
       project: require('path').join(__dirname, './tsconfig.e2e.json')
     });
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
   }
 };
