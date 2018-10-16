26370083b07d65d8a28f65b09979ae97cb7e7167
Vasiliy Malakhov <mvamgc@gmail.com>
2018-10-15 21:11:48 -0400

    #4 - create product - step definitions

--- e2e/steps/create-product-steps.ts
+++ e2e/steps/create-product-steps.ts
@@ -5,32 +5,32 @@ defineSupportCode(({Given, When, Then}) => {
     return 'pending';
   });
 
-  When('user enters random-product-name in product-name field', () => {
+  When(/^user enters random-product-name in product-name field$/, () => {
     // Write code here that turns the phrase above into concrete actions
     return 'pending';
   });
 
-  When('click create-product button', () => {
+  When(/^click create-product button$/, () => {
     // Write code here that turns the phrase above into concrete actions
     return 'pending';
   });
 
-  Then('user sees upload-pictures page', () => {
+  Then(/^user sees upload-pictures page$/, () => {
     // Write code here that turns the phrase above into concrete actions
     return 'pending';
   });
 
-  Then('click skip button', () => {
+  Then(/^click skip button$/, () => {
     // Write code here that turns the phrase above into concrete actions
     return 'pending';
   });
 
-  Then('users sees product-summary page', () => {
+  Then(/^users sees product-summary page$/, () => {
     // Write code here that turns the phrase above into concrete actions
     return 'pending';
   });
 
-  Then('users see previous random-product-name on the screen', () => {
+  Then(/^users see previous random-product-name on the screen$/, () => {
     // Write code here that turns the phrase above into concrete actions
     return 'pending';
   });
